3d216951e30a709c28666bd54df5b0e9815f8c18
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-02-01 05:36:47 +0330

    get csrf

--- phoenix/app/src/main/java/com/khafonline/phoenix/communication/AuthApiClient.java
+++ phoenix/app/src/main/java/com/khafonline/phoenix/communication/AuthApiClient.java
@@ -2,14 +2,23 @@ package com.khafonline.phoenix.communication;
 
 import com.khafonline.phoenix.model.CredentialData;
 import com.khafonline.phoenix.model.LeoResponse;
+import com.khafonline.phoenix.model.SaveLocationData;
 
 import retrofit2.Call;
 
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface AuthApiClient {
     @POST("api/auth/")
     Call<LeoResponse> login(@Body CredentialData data) ;
+
+
+    @GET("android/login/")
+    Call<LeoResponse> get_csrf() ;
+
+    @POST("authentication/save_location/")
+    Call<LeoResponse> save_location(@Body SaveLocationData data) ;
 }
